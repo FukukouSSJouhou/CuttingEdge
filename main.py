1b3fd428025079278518d8deb63370f545a03d3c
kokkiemouse <kokkiemouse@fascode.net>
2022-01-25 21:52:53 +0900

    added

---
+++ main.py
@@ -0,0 +1,12 @@
+# これはサンプルの Python スクリプトです。
+
+# Shift+F10 を押して実行するか、ご自身のコードに置き換えてください。
+# Shift を2回押す を押すと、クラス/ファイル/ツールウィンドウ/アクション/設定を検索します。
+
+def main():
+    print("Tdn")
+# ガター内の緑色のボタンを押すとスクリプトを実行します。
+if __name__ == '__main__':
+    main()
+
+# PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください

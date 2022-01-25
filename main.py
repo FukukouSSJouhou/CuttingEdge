161099f8a954f59de0196e2ae8e28bc64fc1eb22
kokkiemouse <kokkiemouse@fascode.net>
2022-01-25 22:09:58 +0900

    cutting edge

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-# これはサンプルの Python スクリプトです。
-
-# Shift+F10 を押して実行するか、ご自身のコードに置き換えてください。
-# Shift を2回押す を押すと、クラス/ファイル/ツールウィンドウ/アクション/設定を検索します。
-
+import cv2
+import sys
 def main():
     print("Tdn")
-# ガター内の緑色のボタンを押すとスクリプトを実行します。
+    if(len(sys.argv) < 3):
+        print("err")
+        return
+    cv2kun(sys.argv[1],sys.argv[2],True)
+def cv2kun(gazouniki,out_dir,nocolor):
+    cascade_path = './FACE/models/haarcascade_frontalface_default.xml'
+    cascade = cv2.CascadeClassifier(cascade_path)
+    fr = cv2.imread(gazouniki)
+    gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+    front_face_list = cascade.detectMultiScale(gray)
+    print(front_face_list)
+    i=0
+    if len(front_face_list) == 0:
+        print("")
+    else:
+        for (x, y, w, h) in front_face_list:
+            save_path = out_dir + str(i) + '.jpg'
+            if nocolor:
+                img=gray[y:y+h,x:x+w]
+            else:
+                img=fr[y:y+h,x:x+w]
+            cv2.imwrite(save_path, img)
+            i+=1
+    # ガター内の緑色のボタンを押すとスクリプトを実行します。
 if __name__ == '__main__':
     main()
 
